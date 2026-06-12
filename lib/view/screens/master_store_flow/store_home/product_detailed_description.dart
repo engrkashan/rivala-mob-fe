dbc46b9f64bfa10685362d4de7b8d09f2acf590c
engrkashan <kshfkhl@gmail.com>
2026-06-12 12:01:52 +0500

    feat: implement store profile components, post management logic, and UI screens for post interaction and product details

--- lib/view/screens/master_store_flow/store_home/product_detailed_description.dart
+++ lib/view/screens/master_store_flow/store_home/product_detailed_description.dart
@@ -343,9 +343,12 @@ class _ProductDetailedDescriptionState
                                 price: prd.price.toString(),
                                 singlePrice: true,
                                 onTap: () {
-                                  Get.to(() => ProductDetailedDescription(
-                                        product: prd,
-                                      ));
+                                  Get.to(
+                                    () => ProductDetailedDescription(
+                                      product: prd,
+                                    ),
+                                    preventDuplicates: false,
+                                  );
                                 },
                               ),
                             );
